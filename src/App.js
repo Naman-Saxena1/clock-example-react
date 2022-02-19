147d59e675914d4cb3b352625e6f80eae0297a00
Naman-Saxena1 <namansaxena.official@gmail.com>
2022-02-19 19:38:05 +0530

    Added react clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
+let initialTime = new Date().toLocaleString();
+
 function App() {
+  let [time, setTime] = useState(initialTime)
+  function tick()
+  {
+    setTime(new Date().toLocaleString())
+  }
+
+  setInterval(tick, 1000)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Clock</h1>
+      <div>
+          <span id="jsClock">
+            {time}
+          </span>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
